513353212c7b7907ff4f924f218e349b9ebd2845
PC\user <neron_venom@mail.ru>
2017-08-11 21:16:42 +0500

    Создал систему генерации Сетки поля

--- Assets/Scripts/Square_cell_Operator.cs
+++ Assets/Scripts/Square_cell_Operator.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class Square_cell_Operator : MonoBehaviour {
 
-    int[] squre_id = new int[2];
+    int[] square_id = new int[2];
     Transform square_Pos;
     bool barrier;
 
 
-	
+	public void SetCoordinate(int x, int y)
+    {
+        square_id[0] = x;
+        square_id[1] = y;
+    }
 }
--- Assets/Scripts/square_cell_Generator.cs
+++ Assets/Scripts/square_cell_Generator.cs
@@ -3,6 +3,58 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class square_cell_Generator : MonoBehaviour {
+     
+    GameObject square_cell;                  // Сам обьект Единица Сетки
+    GameObject FieldObject;                  // Обьект на котором будет спавнится сетка
+    Vector3 WhereSpawningStarts;           // Точка начала спавна сетки
+    
 
-   
+
+    private void Start()
+    {
+        FieldObject = GameObject.Find("Field");
+        square_cell = GameObject.Find("square_cell");
+        WhereSpawningStarts = square_cell.transform.position;
+        GenerateSquareField(10,10);
+        
+    }
+
+    bool SearchPositionToSpawn() // Проверка Есть ли Начальная позиция для спавна сетки
+    {
+        if(WhereSpawningStarts != null) // Если позиции нет
+        {
+            // Ищем точку для начала спавна
+            return true;
+        }
+        else
+        {
+            return true;
+        }
+
+    }
+    void GenerateSquareField(int x, int y)
+    {
+        if (SearchPositionToSpawn()) //Если имеем обьект для начала спавна то Начинаем
+        {
+           
+
+            for (int x_num = 1; x_num <= x; x_num++)
+            {
+                
+                for (int y_num = 1; y_num <= y; y_num++)
+                {
+                    GameObject square_cell_new = Instantiate(square_cell, WhereSpawningStarts, transform.rotation);
+                    square_cell_new.transform.SetParent(FieldObject.transform);
+                    square_cell_new.GetComponent<Square_cell_Operator>().SetCoordinate(x_num,y_num);
+                    square_cell_new.transform.position = new Vector3(square_cell.transform.position.x + y_num - 1, square_cell.transform.position.y, square_cell.transform.position.z);
+                }
+                square_cell.transform.position = new Vector3(square_cell.transform.position.x, square_cell.transform.position.y, square_cell.transform.position.z - 1);
+            }
+            
+        }
+        else
+        {
+            print("Не имеем позиции для начала спавна Сетки");
+        }
+    }
 }
