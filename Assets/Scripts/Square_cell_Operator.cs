13b02d68febaee7dc5985dff46c2e86e1e10508b
PC\user <neron_venom@mail.ru>
2017-08-11 20:03:07 +0500

    Залил проект в репо

---
+++ Assets/Scripts/Square_cell_Operator.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Square_cell_Operator : MonoBehaviour {
+
+    int[] squre_id = new int[2];
+    Transform square_Pos;
+    bool barrier;
+
+
+	
+}
---
+++ Assets/Scripts/square_cell_Generator.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class square_cell_Generator : MonoBehaviour {
+
+   
+}
