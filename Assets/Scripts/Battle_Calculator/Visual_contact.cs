5228db72100375e433ca2bdfd9b1345dc7ef4951
PC\user <neron_venom@mail.ru>
2017-09-05 20:57:22 +0500

    Зменил некоторые текстурки. Начало системы боя

---
+++ Assets/Scripts/Battle_Calculator/Visual_contact.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Visual_contact : MonoBehaviour {
+
+    Battle_Controller battle_controller;
+    public int distanceToContact;
+
+
+    private void Start()
+    {
+        battle_controller = transform.GetComponentInParent<Battle_Controller>();
+    }
+
+
+    public unit_operator EnemyContact(unit_operator contestUnit)
+    {
+        foreach(unit_operator enemy_unit in battle_controller.unit_list.enemyUnitList)
+        {
+            
+
+        }
+        return null;
+    }
+}
